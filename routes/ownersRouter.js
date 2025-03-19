bc1af785ca2c63e898f5de9a406c861df610d39b
Paritosh Sengar <Paritosh14singh@gmail.com>
2025-03-20 02:32:54 +0530

    model further updates

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owners-model");
 
 router.get("/", (req,res) => {
+    res.send("hello owners its working");
+});
+
+console.log(process.env.NODE_ENV);
+
+router.post("/create", (req,res) => {
     res.send("hello owners its working");
 });
 
