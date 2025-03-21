7aaf0b0d121c1cc9679eeb88c0f762c4f10c7c38
Paritosh Sengar <Paritosh14singh@gmail.com>
2025-03-22 01:13:49 +0530

    frontend files added

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,10 +6,28 @@ router.get("/", (req,res) => {
     res.send("hello owners its working");
 });
 
-console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === "development"){
 
-router.post("/create", (req,res) => {
-    res.send("hello owners its working");
-});
+    router.post("/create", async (req,res) => {
+        let owners = await ownerModel.find();
+        if(owners.length>0) {
+        return res
+         .send(503)
+         .send("you dont have permission to create a new owners");
+    }
+
+       let {fullname, email, password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    });
+       res.status(201).send(createdOwner);
+
+        });
+
+}
+
 
 module.exports = router;
